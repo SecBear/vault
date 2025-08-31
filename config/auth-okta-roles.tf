66d981abbe19c3d9c9a7012f6c814462291a3b95
Bryce Thorpe <thorpebryce@gmail.com>
2025-08-30 23:13:02 -0500

    feat: add okta group-based access mapping to vault policies, cleanup old files

---
+++ config/auth-okta-roles.tf
@@ -0,0 +1,116 @@
+# Okta OIDC Role Mappings
+# Maps Okta groups to Vault policies
+
+# Admin role - for vault-admins group
+resource "vault_jwt_auth_backend_role" "admin" {
+  backend        = vault_jwt_auth_backend.okta_oidc.path
+  role_name      = "admin"
+  token_policies = ["default", "admin"]
+
+  allowed_redirect_uris = [
+    "http://localhost:8200/ui/vault/auth/oidc/oidc/callback",
+    "http://localhost:8250/oidc/callback",
+    "http://127.0.0.1:8200/ui/vault/auth/oidc/oidc/callback"
+  ]
+
+  user_claim   = "email"
+  groups_claim = "groups"
+  oidc_scopes  = ["openid", "email", "profile", "groups"]
+  
+  # Only users in vault-admins group can use this role
+  bound_claims = {
+    groups = "vault-admins"
+  }
+  
+  token_ttl     = 3600
+  token_max_ttl = 7200
+}
+
+# Developer role - for vault-developers group
+resource "vault_jwt_auth_backend_role" "developer" {
+  backend        = vault_jwt_auth_backend.okta_oidc.path
+  role_name      = "developer"
+  token_policies = ["default", "developer"]
+
+  allowed_redirect_uris = [
+    "http://localhost:8200/ui/vault/auth/oidc/oidc/callback",
+    "http://localhost:8250/oidc/callback",
+    "http://127.0.0.1:8200/ui/vault/auth/oidc/oidc/callback"
+  ]
+
+  user_claim   = "email"
+  groups_claim = "groups"
+  oidc_scopes  = ["openid", "email", "profile", "groups"]
+  
+  bound_claims = {
+    groups = "vault-developers"
+  }
+  
+  token_ttl     = 3600
+  token_max_ttl = 7200
+}
+
+# Operations role - for vault-operations group
+resource "vault_jwt_auth_backend_role" "operations" {
+  backend        = vault_jwt_auth_backend.okta_oidc.path
+  role_name      = "operations"
+  token_policies = ["default", "operations"]
+
+  allowed_redirect_uris = [
+    "http://localhost:8200/ui/vault/auth/oidc/oidc/callback",
+    "http://localhost:8250/oidc/callback",
+    "http://127.0.0.1:8200/ui/vault/auth/oidc/oidc/callback"
+  ]
+
+  user_claim   = "email"
+  groups_claim = "groups"
+  oidc_scopes  = ["openid", "email", "profile", "groups"]
+  
+  bound_claims = {
+    groups = "vault-operations"
+  }
+  
+  token_ttl     = 3600
+  token_max_ttl = 7200
+}
+
+# External group aliases to automatically assign policies based on Okta groups
+# This is an alternative approach using identity groups
+
+# Create identity groups that map to policies
+resource "vault_identity_group" "admins" {
+  name     = "admins"
+  type     = "external"
+  policies = ["admin"]
+}
+
+resource "vault_identity_group" "developers" {
+  name     = "developers"
+  type     = "external"
+  policies = ["developer"]
+}
+
+resource "vault_identity_group" "operations" {
+  name     = "operations"
+  type     = "external"
+  policies = ["operations"]
+}
+
+# Create group aliases that map Okta groups to Vault identity groups
+resource "vault_identity_group_alias" "admins" {
+  name           = "vault-admins"  # This should match the Okta group name
+  mount_accessor = vault_jwt_auth_backend.okta_oidc.accessor
+  canonical_id   = vault_identity_group.admins.id
+}
+
+resource "vault_identity_group_alias" "developers" {
+  name           = "vault-developers"
+  mount_accessor = vault_jwt_auth_backend.okta_oidc.accessor
+  canonical_id   = vault_identity_group.developers.id
+}
+
+resource "vault_identity_group_alias" "operations" {
+  name           = "vault-operations"
+  mount_accessor = vault_jwt_auth_backend.okta_oidc.accessor
+  canonical_id   = vault_identity_group.operations.id
+}
--- config/auth-okta.tf
+++ config/auth-okta.tf
@@ -16,7 +16,7 @@ resource "vault_jwt_auth_backend" "okta_oidc" {
   oidc_discovery_url = data.sops_file.oidc_secrets.data["okta_discovery_url"]
   oidc_client_id     = data.sops_file.oidc_secrets.data["okta_client_id"]
   oidc_client_secret = data.sops_file.oidc_secrets.data["okta_client_secret"]
-  
+
   # Set default role
   default_role = "default"
 }
@@ -25,29 +25,28 @@ resource "vault_jwt_auth_backend" "okta_oidc" {
 resource "vault_jwt_auth_backend_role" "default" {
   backend        = vault_jwt_auth_backend.okta_oidc.path
   role_name      = "default"
-  token_policies = ["default", "developer"]  # Assign policies
+  token_policies = ["default", "developer"] # Assign policies
 
   # Redirect URIs for OIDC callback
   allowed_redirect_uris = [
     "http://localhost:8200/ui/vault/auth/oidc/oidc/callback",
-    "http://localhost:8250/oidc/callback",  # CLI callback
+    "http://localhost:8250/oidc/callback", # CLI callback
     "http://127.0.0.1:8200/ui/vault/auth/oidc/oidc/callback"
   ]
 
   # Map the user claim
-  user_claim   = "email"
-  
-  # Remove groups_claim if Okta doesn't provide it
-  # You can re-enable this once you configure Okta to send groups
-  # groups_claim = "groups"
-  
-  # OIDC scopes to request (removed 'groups' since Okta may not support it)
-  oidc_scopes = ["openid", "email", "profile"]
-  
+  user_claim = "email"
+
+  # Ensure that groups are setup in okta before enabling this
+  groups_claim = "groups"
+
+  # OIDC scopes to request
+  oidc_scopes = ["openid", "email", "profile", "groups"]
+
   # Token settings
-  token_ttl     = 3600  # 1 hour
-  token_max_ttl = 7200  # 2 hours
-  
+  token_ttl     = 3600 # 1 hour
+  token_max_ttl = 7200 # 2 hours
+
   # Allow all users from Okta to authenticate
   # Add bound_claims here if you want to restrict to specific users/groups
   # Example:
